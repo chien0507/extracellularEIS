aa9ad1d84ce375968123932531e67c0c16579f5c
chien0507 <130180254+chien0507@users.noreply.github.com>
2025-01-31 11:14:14 -0500

    function files necessary to run calcError.m

---
+++ otherCode/funRCRC.m
@@ -0,0 +1,30 @@
+function zdata = funRCRC(p,w)
+    % this function returns a nx2 matrix response of an equiavlent EIS circuit
+    % 
+    % Inputs:
+    % p = parameters to fit. [r-sol R1 C1 R2 C2]
+    % log base 10 of the parameters for p vector
+    % w = measurement frequencies (rad/s)
+    %
+    % Output:
+    % zdata = nx2 measured impedance at each frequency. Data is in the form
+    % x+i*y. where the data in the first column is the "real" term x, and
+    % the data in the second column is the "imaginary" term y.
+
+    % convert the input signal to the correct "direction"
+    [n,m] = size(w);
+    if n==1 && m~=1
+        w = w';
+    end
+    
+%     global avg_alpha
+%     alpha = avg_alpha;
+    alpha = 1;
+    % p = exp(p);  % used to normalize the magnitude of the contribution between the capacitance and resistance terms
+    % p = 10.^(p);  % used to normalize the magnitude of the contribution between the capacitance and resistance terms
+    Z = p(1) + p(2)./(1+(p(2).*p(3).*1i.*w).^alpha) + p(4)./(1+(p(4).*p(5).*1i.*w).^alpha);
+    
+    zdata(:,1) = real(Z);
+    zdata(:,2) = imag(Z);
+
+end
